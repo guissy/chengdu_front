177ce1713a9ee8cf77eec1dd29261d57ba3b9f74
钟履桂 <zhonglvgui@magicengine.com.cn>
2025-03-27 19:05:54 +0800

    build: next/link next/navigation

--- packages/chengdu_ui/vite.config.ts
+++ packages/chengdu_ui/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
 
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime", "react-icons/fi"],
+      external: ["react", "react-dom", "react/jsx-runtime", "react-icons/fi", "next/link", "next/navigation"],
       output: {
         globals: {
           react: "React",
